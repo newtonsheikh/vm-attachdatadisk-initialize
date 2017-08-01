c3bf1068454fd72539eebeae7135774cd802ddd5
Newton Sheikh <newtonsheikh@hotmail.com>
2017-08-01 15:15:05 +0530

    Added powershell script to initialize disk (#1)

---
+++ BuildRig2/Powershell/InitializeDisk.ps1
@@ -0,0 +1,21 @@
+### Stops the Hardware Detection Service ###
+Stop-Service -Name ShellHWDetection
+
+### Grabs all the new RAW disks into a variable ###
+$disk = get-disk | where partitionstyle -eq 'raw'
+
+### Starts a foreach loop that will add the drive
+### and format the drive for each RAW drive 
+### the OS detects ###
+foreach ($d in $disk){
+$diskNumber = $d.Number
+$dl = get-Disk $d.Number | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize
+Format-Volume -driveletter $dl.Driveletter -FileSystem NTFS -NewFileSystemLabel "OS Disk $diskNumber" -Confirm:$false
+### 2 Second pause between each disk ###
+### Initialization, Partitioning, and formatting ###
+Start-Sleep 2
+}
+### Starts the Hardware Detection Service again ###
+Start-Service -Name ShellHWDetection
+
+### end of script ###
